525dde0a375982c4cc0ed9520a250736d58907b5
personman61 <jeremysl123@gmail.com>
2024-04-18 21:31:40 -0600

    Got it telling me to shut up

--- bot.py
+++ bot.py
@@ -2,14 +2,26 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command(name="parse")
+async def parse(ctx, args):
+    print("$lasher")
+    print(ctx)
+    print(args)
+    await ctx.send("Shut up")
+
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
@@ -19,9 +31,14 @@ async def on_ready():
     )
 
 @client.event
-async def on_typing(channel, user, when):
-    print(channel)
-    print(user)
-    print(when)
+async def on_message(message):
+    # await bot.process_commands(message)
+    print(message)
+    print(message.author)
+    print(message.type)
 
-client.run(TOKEN)
+
+
+
+# client.run(TOKEN)
+bot.run(TOKEN)
